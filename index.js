2c6809f67a24c2c7a69a32c4f711364acd0eb341
RishabMunot <rishabmunot.az@gmail.com>
2020-03-05 02:28:04 +0530

    day 1 wrap

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var db = firebase.firestore();
-
 var username;
 var input = document.getElementById("input");
 input.addEventListener("change", event => {
   username = event["target"].value;
   checkUsername();
 });
-document.querySelector(".button-search").addEventListener("click",(event)=>{
-    username = input.value;
-    checkUsername(username)
-})
+document.querySelector(".button-search").addEventListener("click", event => {
+  username = input.value;
+  checkUsername(username);
+});
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log(user);
+        
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
 
 function checkUsername() {
   console.log(username);
@@ -21,11 +38,58 @@ function checkUsername() {
       if (doc.exists) {
         console.log("Document data:", doc.data());
       } else {
-        
+        fireAuth();
         console.log("No such document!");
       }
     })
     .catch(function(error) {
       console.log("Error getting document:", error);
     });
+}
+
+// FirebaseUI config.
+var uiConfigAfterUsernameFound = {
+  signInSuccessUrl: "",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign("<your-privacy-policy-url>");
+  }
+};
+
+// FirebaseUI config.
+var uiConfigOnLoginButton = {
+  signInSuccessUrl: "index.html",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign("<your-privacy-policy-url>");
+  }
+};
+
+function fireAuth() {
+    document.getElementById("firebaseui-auth-container").setAttribute("style","visibility:visible")
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start("#firebaseui-auth-container", uiConfigOnLoginButton);
 }
